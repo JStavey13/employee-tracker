18b205292308130a882b9e8484402aaf20b9959e
Jartius <JStavey13@users.noreply.github.com>
2023-02-17 23:20:19 -0500

    fixed the schema

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql')
 const inquirer = require('inquirer')
 
-const connection = mysql.createConnection ({
+const db = mysql.createConnection ({
     host: 'localhost',
     user: 'root',
     password: 'rootroot',
     port: 3306,
     database: employee_tracker
 })
+
 
--- db/schema.sql
+++ db/schema.sql
@@ -3,9 +3,29 @@ CREATE DATABASE employee_tracker;
 
 USE employee_tracker;
 
-CREATE TABLE employee_tracker (
+CREATE TABLE departments (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  employee_name VARCHAR(40) NOT NULL,
-  department VARCHAR(30) NOT NULL,
-  employee_role VARCHAR(30) NOT NULL
+  department_name VARCHAR(40) NOT NULL,
+ 
 );
+
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  job_title VARCHAR(40) NOT NULL,
+  job_department VARCHAR(40) NOT NULL,
+  job_salary INT NOT NULL
+ 
+);
+
+CREATE TABLE employees (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(40) NOT NULL,
+  last_name VARCHAR(40) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NOT NULL
+);
+
+
+
+
+
