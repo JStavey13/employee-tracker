6ef862d8e1558f7768f0964b3492d819d12b3e4a
Jartius <JStavey13@users.noreply.github.com>
2023-02-19 22:51:10 -0500

    added foreign keys to schema

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const mysql = require('mysql')
 const inquirer = require('inquirer')
+const express = require('express')
+
 
 const db = mysql.createConnection ({
     host: 'localhost',
     user: 'root',
     password: 'rootroot',
     port: 3306,
-    database: employee_tracker
+    database: 'employee_tracker'
 })
 
+db.query('SELECT * FROM roles', function (err, roles) {
+    console.table(roles);
+    if (err) throw err
+  });
 
+  
--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ USE employee_tracker;
 
 CREATE TABLE departments (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  department_name VARCHAR(40) NOT NULL,
+  department_name VARCHAR(40) NOT NULL
  
 );
 
@@ -14,7 +14,6 @@ CREATE TABLE roles (
   job_title VARCHAR(40) NOT NULL,
   job_department VARCHAR(40) NOT NULL,
   job_salary INT NOT NULL
- 
 );
 
 CREATE TABLE employees (
@@ -22,7 +21,9 @@ CREATE TABLE employees (
   first_name VARCHAR(40) NOT NULL,
   last_name VARCHAR(40) NOT NULL,
   role_id INT NOT NULL,
-  manager_id INT NOT NULL
+  manager_id INT NOT NULL,
+  FOREIGN KEY (role_id)
+  REFERENCES roles(id)
 );
 
 
